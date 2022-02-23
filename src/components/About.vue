48b46ff68dbe93015e11e4aecf863491099ea51d
yuyaamano23 <nba.chako.nba@icloud.com>
2022-02-23 16:15:26 +0900

    vue-router追加

---
+++ src/components/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>About画面</div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Home画面</div>
+</template>
+
+<script>
+export default {};
+</script>
